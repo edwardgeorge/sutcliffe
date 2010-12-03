7961bccc3a839f6160fc2a405772a018dc4f1245
Edward George <edwardgeorge@gmail.com>
2010-12-03 00:14:33 +0000

    get list of available devices from python.

--- sutcliffe/_sutcliffe.c
+++ sutcliffe/_sutcliffe.c
@@ -1,7 +1,30 @@
 #include "Python.h"
 #include "osx.c"
 
+static void
+_get_devices_callback(char *nodename, io_object_t io, void *user_data)
+{
+    PyList_Append((PyObject*)user_data, PyString_FromString(nodename));
+}
+
+static PyObject *
+S_get_devices(PyObject *self, PyObject *args)
+{
+    int num;
+    PyObject *devices = PyList_New(0);
+
+    num = iterate_devices(_get_devices_callback, (void*)devices);
+    if(PyErr_Occurred() != NULL) return NULL;
+    if(num == -1)
+    {
+        PyErr_SetString(PyExc_RuntimeError, "Could not get services.");
+        return NULL;
+    }
+    return devices;
+}
+
 static PyMethodDef SMethods[] = {
+    {"get_devices", S_get_devices, METH_NOARGS},
     {NULL, NULL, 0, NULL} /* sentinel */
 };
 
